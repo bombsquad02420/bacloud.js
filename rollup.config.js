9a11a92b2c61c477173df8b3eb1f165f592be89f
aryan02420 <aryan02420@gmail.com>
2022-07-10 14:34:00 +0000

    🎉 initial commit

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import pluginTypescript from '@rollup/plugin-typescript'
+import pluginDts from 'rollup-plugin-dts'
+import { main, types } from './package.json'
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: main,
+      format: 'esm',
+    },
+    plugins: [pluginTypescript()],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: types,
+      format: 'esm',
+    },
+    plugins: [pluginDts()],
+  },
+]
